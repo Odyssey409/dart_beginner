5e87a4c0955efcb12f093ff491a86cb453dfd98f
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-09-15 00:07:28 +0900

    #2.1 Lists
    dark에서 lists를 선언하는 것은 두 가지 방법이 있다.
    ```dart
    void main(){
    int case1 = [1,2,3,4,5];
    List case2 = [1,2,3,4,5];
    }
    ```
    만약 vscode나 dartPad를 사용한다면 맨 끝을 쉽표로 마무리하면 유용하다.
    ```dart
    void main(){
    int case1 = [
    1,
    2,
    3,
    4,
    5,
    ];
    }
    ```
    
    dart의 유용한 점은 `collection if`와 `collection for`을 지원하는 것이다.
    
    collection if를 사용하면 `존재할 수도 안할 수도 있는 요소를 가지고 올 수 있다.`
    
    ```dart
    void main(){
    var giveMeSix = true;
    int case1 = [
    1,
    2,
    3,
    4,
    5,
    if(giveMeSix) 6,
    ];
    // 아래와 같은 기능이다.
    if(giveMeSix){
    case1.add(6);
    }
    }
    ```

--- main.dart
+++ main.dart
@@ -1,6 +1,18 @@
 void main() {
-  String name = "Odyssey";
-  bool alive = true;
-  int age = 23;
-  double height = 1.85;
+  var number = [1, 2, 3, 4, 5];
+  // 이는 다음과 같음
+  List<int> number1 = [1, 2, 3, 4, 5];
+
+  bool giveMe6 = true;
+
+  var numbers = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    if (giveMe6) 6,
+  ]; // [1, 2, 3, 4, 5, 6]
+
+  print(numbers);
 }
