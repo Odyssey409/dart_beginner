17e98d27330b115feb75508e4d2e40173ce8b86d
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-09-15 00:28:42 +0900

    #2.4 Maps
    Maps
    
    일반적으로 맵은 key와 value를 연결하는 객체입니다. 키와 값 모두 모든 유형의 객체가 될 수 있습니다. 각 키는 한 번만 발생하지만 동일한 값을 여러 번 사용할 수 있습니다.
    ```
    var gifts = {
    // Key: Value
    'first': 'partridge',
    'second': 'turtledoves',
    'fifth': 'golden rings'
    };
    
    // Map 생성자를 사용하여 동일한 객체를 만들 수 있습니다.
    var gifts2 = Map();
    gifts2['first'] = 'partridge';
    gifts2['second'] = 'turtledoves';
    gifts2['fifth'] = 'golden rings';
    ```
    https://dart.dev/guides/language/language-tour#maps

--- main.dart
+++ main.dart
@@ -1,11 +1,31 @@
 void main() {
-  var oldFriends = ['John', 'Doe', 'Jane', 'Doe'];
-  var newFriends = [
-    'Alice',
-    'Bob',
-    'Charlie',
-    'David',
-    for (var friend in oldFriends) "❤️$friend"
+  var player = {
+    'name': 'Messi',
+    'age': 34,
+    'superpower': false,
+  };
+// 아니면 key-value pair의 자료형을 지정하고 사용하여 Map을 생성할 수 있습니다.
+  Map<int, bool> playerSuperpowers = {
+    1: true,
+    2: false,
+    3: true,
+  };
+
+  Map<List<String>, bool> playerSuperpowers2 = {
+    ['run', 'jump']: true,
+    ['fly', 'swim']: false,
+  };
+
+  List<Map<String, Object>> SoccerPlayers = [
+    {
+      'name': 'Messi',
+      'age': 34,
+      'superpower': false,
+    },
+    {
+      'name': 'Ronaldo',
+      'age': 36,
+      'superpower': true,
+    },
   ];
-  print(newFriends);
 }
