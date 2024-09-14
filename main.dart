81d74045cd5277e39bba60b4a8e83f834080539e
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-09-15 00:13:20 +0900

    #2.2 String Interpolation
    변수 사용하는 방법
    
    $달러 기호를 붙이고 사용할 변수를 적어주면 된다.
    만약 무언가를 계산하고 싶다면 ${ } 형태로 적어주면 된다.
    ```
    void main(){
    var name = "tom";
    var age = 10;
    var greeting = "hello $name, I'm ${age + 5}";
    }
    ```

--- main.dart
+++ main.dart
@@ -1,18 +1,6 @@
 void main() {
-  var number = [1, 2, 3, 4, 5];
-  // 이는 다음과 같음
-  List<int> number1 = [1, 2, 3, 4, 5];
-
-  bool giveMe6 = true;
-
-  var numbers = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    if (giveMe6) 6,
-  ]; // [1, 2, 3, 4, 5, 6]
-
-  print(numbers);
+  var name = 'Odyssey';
+  var age = 20;
+  var greeting = 'Hello, I am $name and I\'m ${age + 3}, nice to meet you!';
+  print(greeting);
 }
