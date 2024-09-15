00c6cdec6930a911f29d3b65b57fc3c04fb301c3
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-09-16 00:14:24 +0900

    #3.4 QQ Operator
    ?? 연산자를 이용하면 왼쪽 값이 null인지 체크해서 null이 아니면 왼쪽 값을 리턴하고 null이면 오른쪽 값을 리턴한다.
    ```
    String capitalizeName(String? name) {
    return name?.toUpperCase() ?? "";
    }
    ```
    
    ??= 연산자를 이용하면 변수 안에 값이 null일 때를 체크해서 값을 할당해줄 수 있다.
    ```
    void main() {
    String? name;
    name ??= "sugar";
    name = null;
    name ??= "js";
    print(name); // js
    }
    ```

--- main.dart
+++ main.dart
@@ -1,14 +1,22 @@
-String sayHi(String name, int age, [String? country]) {
-  return 'Hi $name, you are $age years old and you\'re from $country';
-} //optional parameter
+String capitalizeName(String name) => name.toUpperCase();
 
-String sayBye(String name, int age, [String? country = 'Nowhere']) {
-  return 'Bye $name, you are $age years old and you\'re from $country';
-} //optional parameter with default value
+String capitalizeName2(String? name) =>
+    name != null ? name.toUpperCase() : 'Name is null';
+
+// 이런 방식으로 null의 예외처리를 할 수 있다. 하지만 QQ operator를 사용하면 더 짧게 표현할 수 있다.
+
+String capitalizeName3(String? name) => name?.toUpperCase() ?? 'Name is null';
 
 void main() {
-  print(sayHi('Odyssey', 12));
-  print(sayHi('Odyssey', 24, 'Nigeria'));
-  print(sayBye('Odyssey', 12));
-  print(sayBye('Odyssey', 24, 'Nigeria'));
+  print(capitalizeName('john'));
+  print(capitalizeName2('james'));
+  print(capitalizeName3('david'));
+  print(capitalizeName2(null));
+  print(capitalizeName3(null));
+
+  // QQ operator를 사용하여 변수가 null인 경우에 대한 예외처리도 가능하다.
+  String? name;
+  name ??= 'Default Name';
+
+  print(name);
 }
