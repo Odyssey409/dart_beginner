f48fa815e67af6ef857a31f75a9579eea297a2bf
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-09-15 23:50:53 +0900

    #3.1 Named Parameters
    Named parameters
    
    Named parameters는 명시적으로 required로 표시되지 않는 한 선택 사항입니다. 기본값을 제공하지 않거나 Named parameters를 필수로 표시하지 않으면 해당 유형은 기본값이 null이 되므로 null을 허용해야 합니다.
    ```
    String sayHello(
    {required String name, required int age, required String country}) {
    return "${name} / ${age} / ${country}";
    }
    
    void main() {
    print(sayHello(name: "sugar", age: 10, country: "Korea"));
    }
    ```
    https://dart.dev/guides/language/language-tour#parameters

--- main.dart
+++ main.dart
@@ -1,19 +1,31 @@
-void sayHello(String name) {
-  print('Hello $name nice to meet you');
+String sayHi({
+  String name = "anonymous",
+  int age = 0,
+  String country = "Nowhere",
+}) {
+  return 'Hi $name, you are $age years old and you\'re from $country';
 }
 
-String sayHi(String name) {
-  return 'Hi $name nice to meet you';
-  // 함수가 즉시 return하면 return을 생략할 수 있고 화살표 함수로 변경할 수 있다.
-  // String sayHi(String name) => 'Hi $name nice to meet you';
+/* named parameters를 사용하여 함수를 정의하면, 인자가 null이거나 생략될 떄를 대비하여 기본값을 지정해야 한다. 
+이때, 기본값을 지정할 때는 '=' 연산자를 사용한다. 혹은 required를 사용하여 필수 인자로 지정할 수 있다. */
+String sayBye({
+  required String name,
+  required int age,
+  required String country,
+}) {
+  return 'Bye $name, you are $age years old and you\'re from $country';
 }
-
-num add(num a, num b) => a + b;
 
 void main() {
-  sayHello('John');
-  sayHello('Doe');
-  print(sayHi('Jane'));
-  print(sayHi('Doe'));
-  print(add(1, 2));
+  print(sayHi(
+    age: 12,
+    name: 'Odyssey',
+    country: 'Korea',
+  ));
+
+  print(sayBye(
+    age: 23,
+    name: 'Elon',
+    country: 'USA',
+  ));
 }
