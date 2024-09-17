d060eecf1eec49cfe1fedb255b25d3018dfe136e
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-09-17 17:10:46 +0900

    #4.0 Your First Dart Class
    dart에서 property를 선언할 때는 타입을 사용해서 정의한다.
    ```dart
    class Player {
    final String name = 'jisoung';
    final int age = 17;
    void sayName(){
    // class method안에서는 this를 쓰지 않는 것을 권장한다.
    print("Hi my name is $name")
    }
    }
    
    void main(){
    // new를 꼭 붙이지 않아도 된다.
    var player =Player();
    }
    ```

--- main.dart
+++ main.dart
@@ -1,27 +1,14 @@
-typedef ListOfInts = List<int>;
+class Player {
+  final String name = "odyssey";
+  int xp = 1500;
 
-List<int> reverseListOfNumbers(List<int> list) {
-  var reversed = list.reversed;
-  return reversed.toList();
-}
-
-// 위 함수는 아래와 같이 변경할 수 있다.
-
-ListOfInts reverseListOfNumbers2(ListOfInts list) {
-  var reversed = list.reversed;
-  return reversed.toList();
-}
-
-typedef UserInfo = Map<String, String>;
-// 물론 이 방식도 가능하지만, 이처럼 구조체를 사용하는 코드에서는 구조체 대신 클래스를 사용하는 것이 더 좋다.
-String sayHi(UserInfo userInfo) {
-  return 'Hi, ${userInfo['name']} and you are ${userInfo['age']} years old';
+  void sayHello() {
+    print("Hello my name is $name");
+  }
 }
 
 void main() {
-  var list = [1, 2, 3, 4, 5];
-  print(reverseListOfNumbers(list));
-  print(reverseListOfNumbers2(list));
-
-  sayHi({'name': 'Alice', 'age': '30'});
+  var player = Player();
+  print(player.name);
+  player.sayHello();
 }
