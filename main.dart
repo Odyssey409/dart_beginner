fc832887176a3ed04bc5144ae35334e9886cb3b3
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-09-17 18:55:54 +0900

    #4.5 Cascade Notation
    예제를 보자
    ```dart
    // 생략
    void main(){
    var jisoung = Player(name: "jisoung", age: 17, description: "Happy code is end coding");
    jisoung.name = "nico";
    jisoung = 20;
    jisoung.description = "Best Project is End Project";
    }
    ```
    위를 보면 반복되는 부분이 있다. dart에서는 이걸 간단하게 ..으로 해결할 수 있다.
    ```dart
    // 생략
    void main(){
    var jisoung = Player(name: "jisoung", age: 17, description: "Happy code is end coding");
    ...name = "nico"
    ..age = 20
    ..description = "Best Project is End Project";
    }
    ```
    각 '..'들은 jisoung을 가리킨다. 매우 유용한 operator이다.
    앞에 class가 있다면 그 클래스를 가리킨다.

--- main.dart
+++ main.dart
@@ -1,14 +1,14 @@
 class Player {
-  final String name;
+  String name;
   int xp;
   String team;
   int age;
 
-  Player.fromJson(Map<String, dynamic> playerJson)
-      : name = playerJson['name'],
-        age = playerJson['age'],
-        team = playerJson['team'],
-        xp = playerJson['xp'];
+  Player(
+      {required this.name,
+      required this.xp,
+      required this.team,
+      required this.age});
 
   void sayHello() {
     print(
@@ -17,35 +17,8 @@ class Player {
 }
 
 void main() {
-  var apiData = [
-    {
-      "name": "John",
-      "age": 25,
-      "team": "blue",
-      "xp": 100,
-    },
-    {
-      "name": "Doe",
-      "age": 30,
-      "team": "red",
-      "xp": 200,
-    },
-    {
-      "name": "Jane",
-      "age": 35,
-      "team": "blue",
-      "xp": 300,
-    },
-    {
-      "name": "Dane",
-      "age": 40,
-      "team": "red",
-      "xp": 400,
-    },
-  ];
-
-  apiData.forEach((playerJson) {
-    var player = Player.fromJson(playerJson);
-    player.sayHello();
-  });
+  var player1 = Player(name: "odyssey", xp: 100, team: "Red", age: 23)
+    ..name = "Odyssey"
+    ..team = "Blue"
+    ..sayHello();
 }
