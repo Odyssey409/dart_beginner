1c8b7c0b66d1271d1ac127a28aa5f4a31b7fd142
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-09-17 17:26:58 +0900

    #4.2 Named Constructor Parameters
    클래스가 거대해질 경우 위와 같이 생성자 함수를 만드는 것은 비효율적일 것이다.
    예를 들어보자
    ```dart
    class Team {
    final String name;
    int age;
    String description;
    
    Team(this.name, this.age, this.description);
    }
    
    void main(){
    // 헉 너무 많은 인자를 받아야 해서 햇갈린다.
    // 거기다 각 인자의 의미를 알 수가 없다.
    var myTeam = Team("jisoung", 17, "Happy coding is end coding");
    ```
    
    이 문제를 해결할려면 너무 간단하다.
    첫 번째는 중괄호({})를 이용하는 것이다.
    
    ```dart
    class Team {
    final String name;
    int age;
    String description;
    
    Team({this.name, this.age, this.description});
    }
    
    void main(){
    // 한 눈에 볼 수 있다.
    var player = Player(
    name: "jisoung",
    age: 17,
    description: "Happy coding is end coding"
    }
    }
    ```
    두 번째는 name parameter를 사용하는 것이다.
    ```dart
    // 생략
    Player(name:"jisoung", age: 17, description: "Happy coding is end coding");
    ```
    하지만 여기에는 큰 문제가 있다.
    변수가 null일 수도 있기 때문에 우리는 이걸 required를 이용하거나 기본 값을 줘서 처리해 줘야한다. 우리는 required를 사용할 것이다.
    
    ```dart
    // 생략
    Team({
    required this.name,
    required this.age,
    required this.description,
    });,
    ```
    훨씬 좋아졌다.

--- main.dart
+++ main.dart
@@ -1,17 +1,35 @@
 class Player {
   final String name;
   int xp;
+  String team;
+  int age;
 
-  Player(this.name, this.xp);
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.age,
+  });
 
   void sayHello() {
-    print("Hello my name is $name");
+    print(
+        "Hello my name is $name and I am $age years old. My team is $team and I have $xp xp.");
   }
 }
 
 void main() {
-  var player = Player("odyssey", 1500);
+  var player = Player(
+    name: "odyssey",
+    xp: 1500,
+    team: 'red',
+    age: 23,
+  );
   player.sayHello();
-  var player2 = Player("nico", 7500);
+  var player2 = Player(
+    name: "nico",
+    xp: 7500,
+    team: 'blue',
+    age: 25,
+  );
   player2.sayHello();
 }
